c6985c5e9117739f36c018d7fcb804c1fb81a709
shahz <shahz@DESKTOP-A4RB8C0>
2016-09-27 02:58:23 -0400

    pages now highlight

--- assignment1-portfolio/Site.Master.cs
+++ assignment1-portfolio/Site.Master.cs
@@ -11,7 +11,28 @@ namespace assignment1_portfolio
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            SetActivePage();
+        }
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Projects":
+                    projects.Attributes.Add("class", "active");
+                    break;
+                case "Services":
+                    services.Attributes.Add("class", "active");
+                    break;
+                case "About":
+                    about.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
         }
     }
 }
